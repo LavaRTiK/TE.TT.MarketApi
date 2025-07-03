a9219c460074b395a88453805ae4373211068f55
LavaRT <pavlo.legominecraft@gmail.com>
2025-07-03 16:57:42 +0300

    Add Interface

--- TE.TT.MarketApi/Abstarct/IAssetRepositoryService.cs
+++ TE.TT.MarketApi/Abstarct/IAssetRepositoryService.cs
@@ -1,7 +1,9 @@
-﻿namespace TE.TT.MarketApi.Abstarct
+﻿using TE.TT.MarketApi.Model;
+
+namespace TE.TT.MarketApi.Abstarct
 {
     public interface IAssetRepositoryService
     {
-        
+        public Task UpdateAssetRepository(AssetsDto assetsDto);
     }
 }
